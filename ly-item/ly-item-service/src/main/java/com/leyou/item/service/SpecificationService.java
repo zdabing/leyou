d08fe491cca094e4931e0fd97c2c4c8a6c4fccf0
zdabing <979049656@qq.com>
2018-11-20 10:13:52 +0800

    商品规格组查询

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,30 @@
+package com.leyou.item.service;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.pojo.SpecGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Service
+public class SpecificationService {
+
+    @Autowired
+    private SpecGroupMapper specGroupMapper;
+
+    public List<SpecGroup> querySpecGroups(Long cid) {
+        SpecGroup specGroup = new SpecGroup();
+        specGroup.setCid(cid);
+        List<SpecGroup> list = specGroupMapper.select(specGroup);
+        if (CollectionUtils.isEmpty(list)){
+            throw new LyException(ExceptionEnum.SPEC_GROUP_NOT_FOUND);
+        }
+        return list;
+    }
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/SpecificationController.java
@@ -0,0 +1,30 @@
+package com.leyou.item.web;
+
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.service.SpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("spec")
+public class SpecificationController {
+
+    @Autowired
+    private SpecificationService specificationService;
+
+    /**
+     * 根据商品分类查询规格组
+     * @param cid
+     * @return
+     */
+    @GetMapping("groups/{cid}")
+    public ResponseEntity<List<SpecGroup>> querySpecGroups(@PathVariable("cid") Long cid){
+        return ResponseEntity.ok(specificationService.querySpecGroups(cid));
+    }
+}
